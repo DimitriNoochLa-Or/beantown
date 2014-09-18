143893d4080c27d39c96b5aeb9f1acf808ddc574
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2014-09-18 12:25:55 -0700

    added new css

---
+++ javascript/main.js
@@ -0,0 +1,23 @@
+
+$(function(){
+  var header = $("header"),
+      yOffset = 0,
+      triggerPoint = 150;
+  $(window).scroll(function(){
+    yOffset = $(window).scrollTop();
+    
+    if(yOffset >= triggerPoint){
+      header.addClass("minimized");
+    }else{
+      header.removeClass("minimized");
+    }
+    
+  });
+});
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
